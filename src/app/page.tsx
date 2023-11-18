b6a606af0ab79ed9803b88a502b7471bdb47b877
Andrew <andrew@Andrews-MacBook-Pro.local>
2023-11-17 22:22:48 -0600

    hymn project homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,22 @@ export default function Home() {
   const backgroundImage: string = '/TingBackground.jpg';
   const tingVertical: string = '/tingvertical.jpg';
 
+  const hymnProjectDescription = 'Becoming a violinist was not something I sat down and decided to do. It seemed to me rather that once I left that path, I could never go back. Maybe that is why, like a marriage, I still have to say yes to it every day. On days when music feels like a rat race, I try to conjure memories of when I was the most spiritually fulfilled - playing hymns for those who feel them deeply, and laying in tall prairie grass as a child, close to the Earth. This project is for experiencing both.'
+
   return (
     <div className='App'>
       <Parallax bgImage={backgroundImage} strength={600} className='parallax-1'>
         <div className='content'>
-          <div className='title'>Hoi Ting Davidson, Violin</div>
+          <div className='title' id='main-title'>Hoi Ting Davidson, Violin</div>
         </div>
       </Parallax>
 
       <Parallax bgImage={tingVertical} blur={{ min: -5, max: 10 }} strength={300} className='parallax-2'>
         <div className='content'>
-          <div className='title'>Hoi Ting Davidson, Violin</div>
+          <h2 className='title'>The Hymn Project</h2>
+          <h3 className='subtitle'>. . .coming soon</h3>
+          <div className='horizontal-bar'></div>
+          <p className='description-text'>{hymnProjectDescription}</p>
         </div>
       </Parallax>
 
