bc102a8d94a5f63139843401b100c188be3103dd
Charles <dunlapviolin@gmail.com>
2023-12-07 15:46:06 -0600

    contact form progress

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,21 +8,24 @@ export default function ContactForm() {
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
-
+        await fetch('/contact', {
+            method: 'POST',
+            
+        })
     }
 
     return (
-        <div className='contact-form'>
-            <h2 className='title'>Contact Me</h2>
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}></input>
-                <input type='text' placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                <input type='text' placeholder='Subject' value={subject} onChange={(e) => setSubject(e.target.value)}></input>
-                <input type='text' placeholder='Message. . .' value={message} onChange={(e) => setMessage(e.target.value)}></input>
-                <button type='submit'>Submit</button>
+        <div>
+            <h2 className='title' id='contact-title'>Contact Me</h2>
+            <form onSubmit={handleSubmit} className='contact-form'>
+                <input className='form-item' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}></input>
+                <input className='form-item' type='text' placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                <input className='form-item' type='text' placeholder='Subject' value={subject} onChange={(e) => setSubject(e.target.value)}></input>
+                <textarea className='form-item' id='form-message' placeholder='Message. . .' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <button className='form-item' type='submit'>Submit</button>
             </form>
         </div>
     )
