12b89597850e740f842c14a1bf2492b388198c8a
Charles <dunlapviolin@gmail.com>
2024-01-03 20:47:01 -0600

    notated for client use

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -10,6 +10,7 @@ import StudioLocationParallax from '@/components/StudioLocationParallax';
 import StudioContentParallax from '@/components/StudioContentParallax';
 
 export default function Studio() {
+    //Change elements within the '' sections.
     const suzukiQuote: string = '"The main concern for parents should be to bring up their children as noble human beings. That is sufficient. If this is not their greatest hope, in the end the child may take a road contrary to their expectations. Children can play very well. We must try to make them splendid in mind and heart also."';
     const missionStatement1: string = 'I believe in creating an engaging, positive setting which encourages students to approach music and all aspects of their lives with an inquisitive mind. While developing strong technique and a fundamental understanding of music, the lessons of violin will extend far beyond playing the instrument by rote, helping them become more \'noble human beings.\' As a teacher, I am attuned to the different strengths and needs of each student, helping them meet their own goals and encouraging them to reach further as they develop new ones.';
     const missionStatement2: string = 'A life-long learner, my dedication to violin has become an invaluable means of setting aside time to ask questions and challenge myself, skills I seek to share with my studio. I am excited to encourage my students to ask their own questions and to share their music with others.';
