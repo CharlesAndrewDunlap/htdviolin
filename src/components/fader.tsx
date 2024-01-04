12ee8370c4686edff90ec05ec4101fd773e8c0bb
Charles <dunlapviolin@gmail.com>
2024-01-03 22:12:40 -0600

    customized nextConfig for fader component compatability

--- src/components/fader.tsx
+++ src/components/fader.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect } from "react";
 
-export default function Fader() {
+export default function Fader(): JSX.Element {
       //This is used to set the initial body of the page to be a dark grey to avoid FOUC caused by <Parallax> API.
   useEffect(() => {
     document.body.style.opacity = '1';
